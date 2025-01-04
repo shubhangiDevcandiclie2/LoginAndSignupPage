2a2dbefcd2e1e3e374ea05fc0f437f58e3a8f0ce
shubhangiDevcandiclie2 <shubhangi.devcandiclie@gmail.com>
2025-01-04 17:25:01 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -17,11 +17,11 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 
-// MongoDB Connection
-mongoose
-  .connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.error("MongoDB connection failed:", err));
+// // MongoDB Connection
+// mongoose
+//   .connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => console.log("Connected to MongoDB"))
+//   .catch((err) => console.error("MongoDB connection failed:", err));
 
 // Path to React build folder
 const buildPath = path.join(__dirname, "../Frontend/build");
